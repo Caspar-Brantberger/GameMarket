155bb4191e2189fcaf39d2eb3abee5ac22857ee7
Caspar Brantberger <brantbergercaspar@gmail.com>
2026-05-30 20:04:30 +0200

    Fixed listingcard visibility and changed info, also addded pictures and hoover ability on cards

--- frontend/src/features/listings/components/ListingCard.tsx
+++ frontend/src/features/listings/components/ListingCard.tsx
@@ -6,31 +6,30 @@ type ListingCardProps = {
 
 export default function ListingCard({ listing }: ListingCardProps) {
     return (
-        <article className="overflow-hidden rounded-lg border border-gray-200 shadow-md">
+        <article className="overflow-hidden rounded-xl border border-grey-600 bg-gray-900 text-white shadow-sm transition hover:-translate-y-1 hover:border-gray-500 hover:shadow-lg">
             <img
                 src={listing.imageUrl}
                 alt={listing.title}
-                className="h-48 w-full object-cover"
+                className="h-48 w-full object-cover bg-gray-800"
                 />
 
             <div className="space-y-3 p-4">
-                <div>
+                <div className="flex items-start justify-between gap-4 ">
 
                     <h2 className="text-lg font-semibold">{listing.title}
-                        - ${listing.title}
                     </h2>
 
-                    <p className="text-sm text-gray-600">{listing.platform} - {listing.genre}
+                    <p className="text-sm text-gray-500">{listing.platform} - {listing.genre}
                     </p>
                 </div>
 
-                <p className="text-sm text-gray-700">{listing.description}
+                <p className="text-sm text-gray-500">{listing.description}
                 </p>
 
                 <div className="flex items-center justify-between">
                     <span className="text-sm text-gray-500">{listing.condition}
                     </span>
-                    <span className="text-sm font-medium text-gray-900">${listing.price}
+                    <span className="text-sm font-medium text-green-500">${listing.price}
                     </span>
                     <span className="rounded-full bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600">
                         {listing.status}
@@ -43,7 +42,7 @@ export default function ListingCard({ listing }: ListingCardProps) {
                     </span>
                 </div>
 
-                <p className="text-sm text-gray-500">Seller: {listing.sellerName}, Description: {listing.description}</p>
+                <p className="text-sm text-gray-500">Seller: {listing.sellerName}</p>
 
             </div>
         </article>
